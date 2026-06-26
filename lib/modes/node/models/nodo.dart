065f83d97cff91a05845d50f93f68be2aac93aeb
nodespen <nodedpen@gmail.com>
2026-06-26 12:05:59 -0400

    Fix: replace deprecated Color.value with r/g/b accessors

--- lib/modes/node/models/nodo.dart
+++ lib/modes/node/models/nodo.dart
@@ -152,7 +152,12 @@ class Nodo {
 }
 
 extension ColorExtension on Color {
-  String toHex() => '#${value.toRadixString(16).padLeft(8, '0').substring(2)}';
+  String toHex() {
+    final r = (this.r * 255).round().toRadixString(16).padLeft(2, '0');
+    final g = (this.g * 255).round().toRadixString(16).padLeft(2, '0');
+    final b = (this.b * 255).round().toRadixString(16).padLeft(2, '0');
+    return '#$r$g$b';
+  }
 }
 
 Color colorFromHex(String hex) {
